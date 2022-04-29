305089f5b31f7eed4fb9025c254b616452edcf4b
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-04-29 16:49:20 -0300

    aula 2 - instancia e atributos

--- ByteBank/src/bytebank/CriaConta.java
+++ ByteBank/src/bytebank/CriaConta.java
@@ -9,8 +9,11 @@ package bytebank;
  *
  * @author user
  */
-public class ByteBank {
-
+public class CriaConta {
     
-    
+        public static void main(String[] args) {
+    Conta primeiraConta = new Conta();
+    primeiraConta.saldo = 200;
+     System.out.println(primeiraConta.saldo);
+    }
 }
