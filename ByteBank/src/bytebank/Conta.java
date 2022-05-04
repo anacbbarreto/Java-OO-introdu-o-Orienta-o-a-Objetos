46abfe77cb122273348e9c13209c8bf4235035fb
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-05-04 18:56:35 -0300

    métodos com parâmetros e retorno,referência this , referência como parâmetro do método

--- ByteBank/src/bytebank/Conta.java
+++ ByteBank/src/bytebank/Conta.java
@@ -9,16 +9,33 @@ package bytebank;
  *
  * @author user
  */
-public class Conta { 
- 
+  public class Conta { 
     double saldo;
     int agencia;
     int numero;
     String titular;
-    
-    
- public void deposita(double valor) {
-        this.saldo = this.saldo + valor; 
+
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
     }
-    
+
+  public boolean saca(double valor) { 
+    if(this.saldo >= valor) { 
+        this.saldo -= valor; 
+        return true;
+    } else {
+        return false;
+    }
 }
+  
+  public boolean transfere(double valor, Conta destino) { 
+        if(this.saldo >= valor) { 
+            this.saldo -= valor; 
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+  }
+  }
+
