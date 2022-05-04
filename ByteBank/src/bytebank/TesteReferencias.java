7c8ab9df47d6a4ef1d6363cddaa9010883c09241
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-05-04 18:06:08 -0300

    aula 3 - criando metodo e atributo

--- ByteBank/src/bytebank/TesteReferencias.java
+++ ByteBank/src/bytebank/TesteReferencias.java
@@ -9,16 +9,12 @@ package bytebank;
  *
  * @author user
  */
-public class Conta { 
- 
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
-    
-    
- public void deposita(double valor) {
-        this.saldo = this.saldo + valor; 
+public class TesteReferencias {
+
+    public static void main(String [] args) { 
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 300;
+
+        System.out.println("saldo da primeira: " + primeiraConta.saldo);
     }
-    
 }
