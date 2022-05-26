ff3c701325ca45b9b854f88e504138c74e8ce5a7
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-05-26 18:39:27 -0300

    atualização de atributs, get e  set

--- bytebank-encapsulado/src/bytebank/encapsulado/Conta.java
+++ bytebank-encapsulado/src/bytebank/encapsulado/Conta.java
@@ -14,6 +14,17 @@ package bytebank.encapsulado;
     private int agencia;
     private int numero;
     private Cliente titular; 
+    private static int total;
+
+    
+    
+     public Conta(int agencia,int numero) { 
+        Conta.total ++;
+        //System.out.println("o total de contas é " + Conta.total);
+        this.agencia = agencia; 
+        this.numero = numero; 
+        //System.out.println("estou criando uma conta " + this.numero); 
+    }  
 
     public void deposita(double valor) { 
         this.saldo += valor;
@@ -48,19 +59,35 @@ package bytebank.encapsulado;
         return this.numero;
     }
  
- public void setNumero(int novoNumero) { 
+/*public void setNumero(int novoNumero) { 
    
     this.numero = novoNumero; 
 }
+ */
+  public void setNumero(int numero) { 
+        if (numero <= 0) {
+            System.out.println("não pode valor <= 0");
+            return;
+        }
+        this.numero = numero;
+    }
  
  public int getAgencia() {
     return this.agencia;
 }
  
-   public void setAgencia(int agencia){
+  /* public void setAgencia(int agencia){
         this.agencia = agencia;
     }
-
+*/
+ 
+  public void setAgencia(int agencia) { 
+        if (agencia <= 0) { 
+            System.out.println("nao pode valor menor igual a 0");
+            return;
+        }
+        this.agencia = agencia;            
+    }
     public void setTitular(Cliente titular) { 
         this.titular = titular; 
 
@@ -69,4 +96,14 @@ package bytebank.encapsulado;
     public Cliente getTitular() {
         return titular;
     }
+    
+    public static int getTotal() { 
+    return Conta.total; 
+}
+    
+    public static void setTotal(int total) {
+        Conta.total = total;
+    }
+    
+    
 }
--- bytebank-encapsulado/src/bytebank/encapsulado/TestaGetESet.java
+++ bytebank-encapsulado/src/bytebank/encapsulado/TestaGetESet.java
@@ -11,26 +11,26 @@ package bytebank.encapsulado;
  */
 public class TestaGetESet {
    public static void main(String[] args) { 
-        Conta conta = new Conta();
 
-        conta.setNumero(1337);
-        System.out.println(conta.getNumero());
+     //   conta.setNumero(1337);
+      //  System.out.println(conta.getNumero());
 
        Cliente paulo = new Cliente(); 
         paulo.setNome("paulo carlos");
 
-        conta.setTitular(paulo);
+     //   conta.setTitular(paulo);
 
-        System.out.println(conta.getTitular().getNome());
+     //   System.out.println(conta.getTitular().getNome());
 
-        conta.getTitular().setProfissao("programador"); 
+      //  conta.getTitular().setProfissao("programador"); 
         //agora em duas linhas: 
-        Cliente titularDaConta = conta.getTitular();
+     /*   Cliente titularDaConta = conta.getTitular();
         titularDaConta.setProfissao("programador");
        System.out.println(titularDaConta); 
         System.out.println(paulo);
         System.out.println(conta.getTitular());
 
-
+*/
   }
 }
+
